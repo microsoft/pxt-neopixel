f614a5b0a0067bef11b04ba2aabaa3f5a839e552
Peli de Halleux <jhalleux@microsoft.com>
2017-01-24 20:52:27 -0800

    scoping tests

--- neotest.ts
+++ neotest.ts
@@ -1,49 +1,50 @@
-let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB);
+{
+    let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB);
 
-strip.showColor(NeoPixelColors.Red)
-basic.pause(2000)
-strip.showColor(NeoPixelColors.Green)
-basic.pause(1000)
-for (let i = 0; i <= strip.length(); i++) {
-    strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Blue))
-    strip.show()
-    basic.pause(100)
-}
-for (let i = 0; i <= strip.length(); i++) {
-    strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Green))
-    strip.show()
-    basic.pause(100)
-}
-let sub = strip.range(10, 20)
-sub.showColor(NeoPixelColors.Yellow);
-basic.pause(200);
-
-sub.showBarGraph(5, 10);
-basic.pause(200);
-
-let br = 100;
-strip.setBrigthness(100);
-input.onButtonPressed(Button.B, () => {
-    br = br + 20;
-    if (br > 255) {
-        br = 5;
+    strip.showColor(NeoPixelColors.Red)
+    basic.pause(2000)
+    strip.showColor(NeoPixelColors.Green)
+    basic.pause(1000)
+    for (let i = 0; i <= strip.length(); i++) {
+        strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+        basic.pause(100)
     }
-    strip.setBrigthness(br);
-});
+    for (let i = 0; i <= strip.length(); i++) {
+        strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+        basic.pause(100)
+    }
+    let sub = strip.range(10, 20)
+    sub.showColor(NeoPixelColors.Yellow);
+    basic.pause(200);
 
-let rotationMode = false;
-input.onButtonPressed(Button.A, () => {
-    rotationMode = !rotationMode;
-    if (rotationMode) {
-        basic.showLeds(`
+    sub.showBarGraph(5, 10);
+    basic.pause(200);
+
+    let br = 100;
+    strip.setBrigthness(100);
+    input.onButtonPressed(Button.B, () => {
+        br = br + 20;
+        if (br > 255) {
+            br = 5;
+        }
+        strip.setBrigthness(br);
+    });
+
+    let rotationMode = false;
+    input.onButtonPressed(Button.A, () => {
+        rotationMode = !rotationMode;
+        if (rotationMode) {
+            basic.showLeds(`
             . # # # .
             # . . . #
             # . . . #
             # . . . #
             . # # # .
             `);
-    } else {
-        basic.showLeds(`
+        } else {
+            basic.showLeds(`
             . . # . .
             . . . # .
             # # # # #
@@ -51,20 +52,20 @@ input.onButtonPressed(Button.A, () => {
             . . # . .
             `);
 
-    }
-});
+        }
+    });
 
-while (true) {
-    let x = input.acceleration(Dimension.X) / 2
-    let y = input.acceleration(Dimension.Y) / 2
-    let z = input.acceleration(Dimension.Z) / 2
-    if (rotationMode) {
-        strip.rotate();
-    } else {
-        strip.setPixelColor(0, neopixel.rgb(x, y, -z));
-        strip.shift(1);
+    while (true) {
+        let x = input.acceleration(Dimension.X) / 2
+        let y = input.acceleration(Dimension.Y) / 2
+        let z = input.acceleration(Dimension.Z) / 2
+        if (rotationMode) {
+            strip.rotate();
+        } else {
+            strip.setPixelColor(0, neopixel.rgb(x, y, -z));
+            strip.shift(1);
+        }
+        strip.show();
+        basic.pause(100);
     }
-    strip.show();
-    basic.pause(100);
 }
-
